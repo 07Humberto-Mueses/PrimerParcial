15104f8fe09d89ab8afa25813e8c9942ee4b1b63
Humberto <humbertom0798@gmail.com>
2025-03-24 23:12:17 -0500

    Completado el capitulo 11 y funcionando bien

---
+++ app/query/route.ts
@@ -0,0 +1,6 @@
+// export function listInvoices(){
+//   SELECT invoices.amount, customers.name
+//   FROM invoices
+//   JOIN customers ON invoices.customer_id = customers.id
+//   WHERE invoices.amount = 666;
+// }
