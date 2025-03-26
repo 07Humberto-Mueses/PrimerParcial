5b27e7d722082a8cac92632443d2f1ccaa40a51e
Humberto <humbertom0798@gmail.com>
2025-03-25 21:13:59 -0500

    para ok vercel

--- app/query/route.ts
+++
@@ -1,6 +0,0 @@
-// export function listInvoices(){
-//   SELECT invoices.amount, customers.name
-//   FROM invoices
-//   JOIN customers ON invoices.customer_id = customers.id
-//   WHERE invoices.amount = 666;
-// }
